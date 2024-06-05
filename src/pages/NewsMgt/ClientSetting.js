40cd6b27e1daa1bc813c1d92e9cc4cc3e4898904
US <usw@info.com>
2024-06-05 01:00:19 -0700

    recovered norris's work

--- src/pages/NewsMgt/ClientSetting.js
+++ src/pages/NewsMgt/ClientSetting.js
@@ -1,23 +1,76 @@
-import React from "react";
-import { Col, Container, Row } from "reactstrap";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, CardHeader, Col } from 'reactstrap';
+import { getClientLists } from '../../helpers/backend_helper';
+
 
 const ClientSetting = () => {
-    document.title ="Meme Analyzer | Client Settings";
+    const [clientList, setClientList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getClientLists();
+            setClientList(response);
+        }
+        fetchData();
+    }, []);
+
+    document.title = "Meme Analyzer | Client Settings";
     return (
         <React.Fragment>
             <div className="page-content">
-                <Container fluid>
-                    <Row>
-                        <Col>
-                            <div className="h-100">
-                                <Row>
-                                    Client Settings Page
-                                </Row>
+                <Col>
+                    <Card>
+                        <CardHeader className="align-items-center d-flex">
+                            <h4 className="card-title mb-0 flex-grow-1">Client Settings</h4>
+                        </CardHeader>
+
+                        <CardBody>
+                            <div className="table-responsive table-card">
+                                <table className="table table-hover table-centered table-success align-middle table-nowrap mb-0">
+                                    <thead className="text-muted table-light">
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Twitter ID</th>
+                                        <th scope="col">Website</th>
+                                        <th scope="col">Telegram ID</th>
+                                        <th scope="col">Twitter Followers</th>
+                                        <th scope="col">Telegram Subscribers</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {(clientList || []).map((item, key) => (
+                                        <tr key={key}>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.id}</h5>
+                                            </td>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.name}</h5>
+                                            </td>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.twitter_id}</h5>
+                                            </td>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.website}</h5>
+                                            </td>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.telegram_id}</h5>
+                                            </td>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.twitter_followers}</h5>
+                                            </td>
+                                            <td>
+                                                <h5 className="fs-14 my-1 fw-normal">{item.telegram_subscribers}</h5>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </table>
                             </div>
-                        </Col>
-                        {/* <RecentActivity /> */}
-                    </Row>
-                </Container>
+                        </CardBody>
+                    </Card>
+                </Col>
             </div>
         </React.Fragment>
     );
